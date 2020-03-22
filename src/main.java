ddb7ee0c423bd90eb872b3f85a6596d015bd7cda
Bastichek <62508620+Bastichek@users.noreply.github.com>
2020-03-22 18:24:49 +0300

    Writing to a file

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -8,6 +9,7 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) throws IOException {
+        FileWriter writer = new FileWriter("result.txt", false);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите данные для поиска:");
         String sear = scanner.nextLine();
@@ -17,9 +19,11 @@ public class main {
         List<String> lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
         for (int i = 0; i < lines.size(); i++) {
             if (lines.get(i).contains(sear)) {
+                writer.write(lines.get(i) + "\n");
                 System.out.println("Найдено в строке: " + i);
             }
         }
+        writer.close();
     }
 
 }
