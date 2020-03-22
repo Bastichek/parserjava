dbebf04d44b792193cdd02f7d9b6eec5b8e89588
Bastichek <62508620+Bastichek@users.noreply.github.com>
2020-03-22 18:57:54 +0300

    Reading files from a folder

--- src/main.java
+++ src/main.java
@@ -1,10 +1,13 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 
 public class main {
@@ -16,11 +19,17 @@ public class main {
         System.out.println("Введите путь:");
         String path = scanner.next();
 
-        List<String> lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
-        for (int i = 0; i < lines.size(); i++) {
-            if (lines.get(i).contains(sear)) {
-                writer.write(lines.get(i) + "\n");
-                System.out.println("Найдено в строке: " + i);
+        List<Path> files = Files.walk(Paths.get(path))
+                .filter(Files::isRegularFile)
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < files.size(); i++) {
+            List<String> lines = Files.readAllLines(files.get(i), StandardCharsets.UTF_8);
+            for (int j = 0; j < lines.size(); j++) {
+                if (lines.get(j).contains(sear)) {
+                    writer.write(lines.get(j) + "\n");
+                    System.out.println("Найдено в строке: " + j);
+                }
             }
         }
         writer.close();
