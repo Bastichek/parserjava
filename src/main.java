e4030af5601b44717c12621a559288b7639cca2a
Bastichek <62508620+Bastichek@users.noreply.github.com>
2020-03-25 17:21:18 +0300

    Changed working with file and some renaming

--- src/main.java
+++ src/main.java
@@ -1,6 +1,4 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,8 +8,10 @@ import java.util.stream.Collectors;
 
 public class main {
     public static void main(String[] args) {
+
         String filePath = null;
         String sear = null;
+
         try {
             filePath = args[0];
             sear = args[1];
@@ -26,41 +26,40 @@ public class main {
             e.printStackTrace();
         }
 
-
         List<Path> files = null;
         try {
             files = Files.walk(Paths.get(filePath))
                     .filter(Files::isRegularFile)
                     .collect(Collectors.toList());
         } catch (IOException e) {
-            System.out.println("Error! Enter arguments! (folderPath)");
+            System.out.println("Error! Wrong argument! (folder)");
         }
 
-        for (Path file : files) {
-            System.out.println("File: " + file);
+        for (Path path : files) {
+
+            System.out.println("File: " + path);
             try {
-                writer.write("File:" + file + "\n");
+                writer.write("File:" + path + "\n");
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            List<String> lines = null;
+
+            String line;
+            BufferedReader reader;
             try {
-                lines = Files.readAllLines(file, StandardCharsets.UTF_8);
-            } catch (IOException e) {
-                System.out.println("Error! Enter arguments! (filePath)");
-            }
-
-            for (String str : lines) {
-                if (str.contains(sear)) {
-                    try {
-                        writer.write(str + "\n");
-                        System.out.println("Find in: " + (lines.indexOf(str) + 1) + " / " + lines.size());
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                reader = new BufferedReader(new FileReader(String.valueOf(path)));
+                int i = 1;
+                while ((line = reader.readLine()) != null) {
+                    if (line.contains(sear)) {
+                        writer.write(line + "\n");
+                        System.out.println("Find in: " + (i));
                     }
-                }
+                    i++;
 
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
         try {
@@ -68,6 +67,7 @@ public class main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
 }
